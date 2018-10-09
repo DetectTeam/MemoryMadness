9ae31bd6f31ea27703999f4a49e9017e5596d4c2
Eoghan <gallageo@tcd.ie>
2018-10-09 10:20:48 +0100

    Added Memory Phase

---
+++ Assets/CountDown.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CountDown : MonoBehaviour 
+{
+
+	[SerializeField] private int timeLeft;
+	[SerializeField] private TextMeshProUGUI timer;
+
+	[SerializeField] private GameObject game;
+	// Use this for initialization
+	private void Start () 
+	{
+		StartCoroutine( Sequence() );
+	}
+
+	private IEnumerator Sequence()
+	{
+		yield return new WaitForSeconds( 1.0f );
+
+		while( timeLeft > 0 )
+		{
+			yield return new WaitForSeconds( 1.0f );
+			timeLeft --;
+			timer.text = timeLeft.ToString();
+		}
+
+		game.SetActive( true );
+
+
+	}
+	
+	
+}
