024fc55f6c2b78f7d574b3826dd8ecc896c90e59
Eoghan <eoghan.gallagher@gmail.com>
2018-10-22 14:47:51 +0100

    Levels Update

---
+++ Assets/MemoryPhaseContainer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryPhaseContainer : MonoBehaviour 
+{
+
+
+	[SerializeField] private GameObject[] phases;
+	[SerializeField] private float delay;
+
+	private void OnEnable()
+	{
+		StartCoroutine( LoadPhase() );
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+
+	private IEnumerator LoadPhase()
+	{
+		yield return new WaitForSeconds( delay );
+
+		int currentPhase = 2;
+		//Check the level count
+
+		if( PlayerPrefs.HasKey( "CurrentLevel" ) )
+		{
+			currentPhase = PlayerPrefs.GetInt( "CurrentLevel" );
+		}
+		
+		phases[ currentPhase ].SetActive( true );
+		//Based on level count set correct phase to active
+	}
+	
+	
+}
