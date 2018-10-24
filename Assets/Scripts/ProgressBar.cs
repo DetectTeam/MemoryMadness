9d1653c9d532eabd03914515d848f0b8112a1ede
Eoghan Gallagher <eoghan.gallagher@gmail.com>
2018-10-24 12:15:45 +0100

    Added progress bar to results screen

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ProgressBar : MonoBehaviour 
+{
+	[SerializeField] private Image progressBar;
+	[SerializeField] private TextMeshProUGUI percentageText;
+
+	private int speed = 1;
+	
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine( Fill() );
+	}
+
+	private IEnumerator Fill()
+	{
+		float currentValue = 0;
+
+		yield return new WaitForSeconds( 2.0f );
+		
+		while( currentValue < 100 )
+		{
+			yield return null;
+			currentValue ++; //speed; //* Time.deltaTime;
+			percentageText.text = currentValue.ToString()  +  "%";
+			progressBar.fillAmount = ( currentValue / 100 );
+
+			
+		}
+	}
+	
+	
+}
