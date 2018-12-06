08440f8bc166b779f81b3062e59a900fa5453400
Eoghan Gallagher <eoghan.gallagher@gmail.com>
2018-12-06 12:27:52 +0000

    Added Session Class

---
+++ Assets/Scripts/Sessions/Session.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MemoryMadness
+{
+  
+
+	public class StudyItem 
+	{
+		public string Shape { get; set; }
+		public string Colour{ get; set; }
+	}
+
+	public class TestSlot : StudyItem
+	{
+		public string Type { get; set; }
+		public string StudyOrder { get; set; }
+	}
+
+	public class Session  
+	{
+		public int Level { get; set; } // level 1 - 8
+		public string UserID { get; set; } 
+		public int Stage { get; set; } // Stage 1 - 4
+		public int SymbolArraySize{ get; set; } //Num of symbols to memorize 2 - 5
+		public int TrialNumber { get; set; } // a total of 32 trials
+		public string Condition { get; set; } //Binding or Shape
+		public string ShapeType { get; set; } //Abstract or Nameable
+		public int DistractorCount { get; set; } //18 - 15 
+		public int SumAccuracy { get; set; }
+		public int NormalErrors { get; set; } 
+		public int LureErrors { get; set; }
+		public int TotalLives { get; set; } //2 - 5 based on stage
+		public int LivesLost { get; set; }
+		public List<StudyItem> StudyItems; //Items in MemoryPhase
+		public List<TestSlot> TestSlots; //Record of items in the test phase
+		public List<int> AccuracySlots; // List of player decisions for each slot
+		public List<int> OrderSlot;
+		public List<float> TimeSlot;
+	}
+}
