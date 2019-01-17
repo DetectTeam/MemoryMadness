52e677d1e7e2a0019eec315c0e67569aa6002713
Eoghan Gallagher <eoghan.gallagher@gmail.com>
2019-01-17 15:48:28 +0000

    Added Tutorial Scene

---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour 
+{
+	public void LoadScene( string sceneName )
+	{
+		if( sceneName.Length > 0 )
+			SceneManager.LoadSceneAsync( sceneName );
+		else
+			Debug.Log( "Not a valid scene Name" );
+	}	
+}
